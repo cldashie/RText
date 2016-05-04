88f4509cfd3cf80e408d07ca9d5057395895de0d
cldashie <cldashie@ncsu.edu>
2016-05-04 16:37:01 -0400

    Updated code with Chapter 5 of text exercises

--- Chapter2.R
+++ Chapter2.R
@@ -1,1 +1,20 @@
-# Creating a new script as a test of the Git function for RStudio
+# Chapter 5: Advanced Data Structures
+
+# Creating a Dataframe
+x <- 10:1
+y <- -4:5
+q <- c("Hockey", "Football", "Baseball", "Curling", "Rugby", 
+       "Lacrosse", "Basketball", "Tennis", "Cricket", "Soccer")
+theDF <- data.frame(x, y, q)
+theDF
+
+# Assigning names to the data frame
+theDF <- data.frame( First = x, Second = y, Sport = q)
+theDF
+
+# Checking the attributes of a data frame
+nrow(theDF)
+ncol(theDF)
+dim(theDF)
+
+# Checking the names of a data frame
