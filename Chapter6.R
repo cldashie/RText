14f488e05b4758121196bd2e9cead9887e3851c8
cldashie <cldashie@ncsu.edu>
2016-05-05 19:17:42 -0400

    Finished Chapter 6

--- Chapter6.R
+++ Chapter6.R
@@ -8,3 +8,13 @@ theUrl <- "http://www.jaredlander.com/data/Tomato%20First.csv"
 tomato <- read.table (file = theUrl, header = TRUE, sep = ",")
 
 head(tomato)
+
+# One helpful argument is stringsAsFactors. Setting this as 
+# false prevents character columns from being converted to factor 
+# columns which saves computation time. It can also be used in data.frames.
+x <- 10:1
+y <- -4:5
+q <- c("Hockey", "Football", "Baseball", "Curling", "Rugby", "Lacrosse",
+       "Basketball", "Tennis", "Cricket", "Soccer")
+theDF <- data.frame(Frist=x, Second=y, Sport=q, stringsAsFactors = FALSE)
+theDF$Sport
